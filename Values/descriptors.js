6226c655e91a3a29eae372995341663f668ccf5d
Jacob Amisano <jamisano2@huskers.unl.edu>
2022-09-28 15:04:46 -0500

    Added basic values for clothing and descriptors

---
+++ Values/descriptors.js
@@ -0,0 +1,4 @@
+export const descriptors = ['shoddly spray painted in green and blue semicircles', 'covered in half eaten oranges', 'with googly eyes all over',
+  'covered in off brand crayons', 'covered in off brand cereal titles', 'with a love letter stapled to the back', 
+  'filled to the brim with straws', 'with a bunch of stuffed pidgeons staped to it', 'with a best buy name tag',
+  'with a angry frowning face cut out of it', ]
---
+++ Values/dress-in.js
@@ -0,0 +1,5 @@
+export const tops = ['a t-shirt', ' a polo', 'a top half of a donkey as a top', 'a bottom half of a donkey as a shirt', 'a shirt filled with ketchup spots', 
+  'an ugly sweater', 'a giant cardboard box top', 'a giant milk container top', 'a half-eaten pickle top', 'a giant polly pocket top', 'a rabid jellyfish costume top',
+  'a santa costume shirt', 'a Grammy Norma shirt', 'a shirt with the word shumple on it', 'a power ranger top', 'suit top', 'an emo croptop',
+  'a mecha suit top', 'a traditional japanese kimono', 'an indian Sari', 'a giant teddy bear top', 'a straightjacket'];
+  
